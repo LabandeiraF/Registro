fe4f97f4a97d686ea78171ec41978c82f6565e79
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-08-12 18:24:56 -0300

    agregamos la solucion a la validacion de campos

--- js/validacion.js
+++ js/validacion.js
@@ -19,3 +19,42 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+/*validacion de Campos*/
+
+function passwordIgualdad(){
+  return document.getElementById('password1').value === document.getElementById('password2').value
+  }
+  
+  function passwordlongitud(){
+    const minimo = 6;
+      return document.getElementById('password1').value.length >= minimo
+  }
+  
+    let contador = 0 
+  
+  function validacioncampos (){
+  let validacion = document.getElementsByClassName('form-control')
+  
+  for (let i=0; i<validacion.length; i++){
+  
+      if (validacion[i].value === ""){
+          contador++
+}
+  }
+  console.log(contador)
+  
+  return contador  == 0
+  }
+  
+  
+  document.getElementById('regBtn').addEventListener("click", function() {
+  
+  if (passwordIgualdad() && passwordlongitud() && validacioncampos ()){
+      showAlertSuccess()
+  }
+  else{
+      showAlertError()
+  }
+  
+})
+
